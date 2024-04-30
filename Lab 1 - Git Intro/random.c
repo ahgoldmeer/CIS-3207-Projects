ee06904286d0f92fe2dfc3c830d52b6c93dcf16f
Amitai Goldmeer <95256721+ahgoldmeer@users.noreply.github.com>
2024-04-29 20:25:36 -0400

    Add files via upload

---
+++ Lab 1 - Git Intro/random.c
@@ -0,0 +1,5 @@
+char randchar(){
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int random = rand() % 26;
+    return alphabet[random];
+}
